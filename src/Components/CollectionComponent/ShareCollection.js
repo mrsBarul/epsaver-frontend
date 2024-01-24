f09f2b31bb7357ff36884d8c8a53eab70367d9fb
mrsBarul <dobro.ve@mail.ru>
2024-01-24 05:42:11 +0300

    update share

--- src/Components/CollectionComponent/ShareCollection.js
+++ src/Components/CollectionComponent/ShareCollection.js
@@ -1,50 +1,73 @@
 import { jsPDF } from 'jspdf';
 import { getAllSerials } from '../../Fetch/FetchSerial';
-import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { getUserData } from '../../Redux/authSlice';
-import Collection from './Collection';
+import  Roboto from "./fonts/Roboto.ttf"
 
 const ShareCollection = () => {
+  const [allSerials, setAllSerials] = useState([]);
+  console.log(allSerials)
+  const userData = useSelector(getUserData);
+  const userId = userData.user.id;
 
-    const [ allSerials, setAllSerials ] = useState([])
-    const userData = useSelector(getUserData)
-    const userId = userData.user.id;
+  const shareCollection = async () => {
 
-    const shareCollection = () => {
+        getAllSerials(setAllSerials, userId);
+        
         const pdf = new jsPDF();
-        {allSerials.length > 0 ? allSerials.map(serial => pdf.text(
-            <Collection key={serial._id} 
-            id={serial._id}
-            title={ serial.title }
-            translate={ serial.translate }
-            series={ serial.series }
-            episode={ serial.episode }
-            status={ serial.status }
-            poster={serial.poster}
-            raiting={serial.raiting}
-            comment={serial.comment}
-            newSeason={serial.newSeason}
-            setAllSerials={ setAllSerials }
-            />
-            )): 
-        <div className='emptyCollection'>
-            <p>Ваша коллекция пуста!</p>
-        </div>}
+        let yPos = 20;
 
-        pdf.save('serials_collection.pdf');
-    }
+        pdf.addFileToVFS("Roboto.ttf", Roboto);
+        pdf.addFont("Roboto.ttf", "Roboto", "normal");
+        pdf.setFont("Roboto");
+        pdf.text("Привет!", 100, 10);
 
-    useEffect(() => {
-        getAllSerials(setAllSerials, userId)
-    }, [setAllSerials, userId])
+      allSerials.forEach((serial, index) => {
+        if (serial.poster) {
+          const imgData = serial.poster; 
+          pdf.addImage(imgData, 'JPEG', 20, yPos, 50, 50);
+        } else return undefined
 
-    return (
-        <button onClick={ shareCollection }
-        className="updateShare buttonUpdateShare">
-            ПОДЕЛИТЬСЯ КОЛЛЕКЦИЕЙ
-        </button>
-    )
+
+        const title = serial.title;
+        const series = serial.series;
+        const episode = serial.episode;
+        const status = serial.status;
+        const raiting = serial.raiting;
+        const comment = serial.comment;
+        console.log(title)
+
+        pdf.text("Привет", 20, 20)
+        pdf.text(title, 80, yPos + 10);
+        pdf.text(`Серия: ${ series } Эпизод: ${ episode }`, 80, yPos + 20);
+        pdf.text(`Статус: ${status}`, 80, yPos + 30);
+        pdf.text(`Рейтинг: ${raiting}`, 80, yPos + 40);
+        pdf.text(`Комментарий: ${ comment }`, 80, yPos + 50);
+
+        // Увеличиваем Y позицию для следующего сериала
+        yPos += 60;
+
+        // Добавляем отступ между сериалами
+        if (index < allSerials.length - 1) {
+          yPos += 10;
+        }
+      });
+
+      // Сохраняем PDF
+      pdf.save('serials_collection.pdf');
+    
+  };
+
+  useEffect(() => {
+    getAllSerials(setAllSerials, userId);
+  }, [setAllSerials, userId]);
+
+  return (
+      <button onClick={shareCollection} className="updateShare buttonUpdateShare">
+        ПОДЕЛИТЬСЯ КОЛЛЕКЦИЕЙ
+      </button>
+  );
 }
 
 export default ShareCollection;
