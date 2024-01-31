0b910a27159db0ffad5b7d18a96480252ed9e81a
mrsBarul <dobro.ve@mail.ru>
2024-02-01 00:08:58 +0300

    testing pdfmake

--- src/Components/CollectionComponent/ShareCollection.js
+++ src/Components/CollectionComponent/ShareCollection.js
@@ -1,29 +1,84 @@
 import { useSelector } from "react-redux";
-import { getIsAuthenticated, getUserData } from "../../Redux/authSlice";
-import { useState } from "react";
+import { getUserData } from "../../Redux/authSlice";
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { useEffect, useState } from "react";
+import { getAllSerials } from "../../Fetch/FetchSerial";
 
 const ShareCollection = () => {
-
-  const [collectionLink, setCollectionLink] = useState('');
-  const isAuthenticated = useSelector(getIsAuthenticated);
+  pdfMake.vfs = pdfFonts.pdfMake.vfs;
+  const [allSerials, setAllSerials] = useState([]);
   const userData = useSelector(getUserData);
   const userId = userData.user.id;
 
-  const handleShare = () => {
-    if (isAuthenticated && userData && userData.user && userData.user.isActivated) {
-      const collectionUserUrl = `/collection/${userId}`;
-      setCollectionLink(collectionUserUrl);
-      window.location.href = collectionLink
-    } else {
-      window.location.href = '/ChoiceAuth'
+  const handleShare = async () => {
+    const documentDefinition = {
+      content: [
+        { text: 'Моя коллекция', style: 'mainHeader' }
+      ],
+      styles: {
+        mainHeader: {
+          fontSize: 24,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        poster: {
+          margin: [0, 10, 0, 10], 
+          width: 100,
+          height: 150,
+        },
+      },
+    };
+
+    const convertImageToDataURL = async (imageUrl) => {
+      return new Promise((resolve, reject) => {
+        fetch(imageUrl)
+          .then(response => response.blob())
+          .then(blob => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+              resolve(reader.result);
+            };
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+          })
+          .catch(reject);
+      });
+    };
+
+    for (const serial of allSerials) {
+      const base64Image = await convertImageToDataURL(serial.poster);
+      console.log(base64Image)
+      
+      documentDefinition.content.push({
+        text: [
+          { text: 'Название: ', bold: true }, serial.title, '\n',
+          { text: 'Перевод: ', bold: true }, serial.translate, '\n',
+          { text: 'Серии: ', bold: true }, serial.series, '\n',
+          { text: 'Эпизоды: ', bold: true }, serial.episode, '\n',
+          { text: 'Статус: ', bold: true }, serial.status, '\n',
+          { text: 'Рейтинг: ', bold: true }, serial.raiting, '\n',
+          { text: 'Комментарий: ', bold: true }, serial.comment, '\n',
+          {image: `'${base64Image}'`, style: 'poster',}
+        ],
+        margin: [0, 0, 0, 20],
+      });
     }
+
+    pdfMake.createPdf(documentDefinition).download('моя_коллекция.pdf');
   };
 
+  useEffect(() => {
+    getAllSerials(setAllSerials, userId);
+  }, [setAllSerials, userId]);
+
   return (
-      <button onClick={ handleShare } className="updateShare buttonUpdateShare">
+    <div>
+      <button onClick={handleShare} className="updateShare buttonUpdateShare">
         ПОДЕЛИТЬСЯ КОЛЛЕКЦИЕЙ
       </button>
+    </div>
   );
-}
+};
 
 export default ShareCollection;
